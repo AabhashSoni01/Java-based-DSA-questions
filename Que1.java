9c2c40cacfc93a7438402cf0071b5b72872d82fe
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-05 23:46:13 +0530

    arrayQue

---
+++ Que1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Que1 {
+
+    // Find the maximum and minimum element in an array
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        // sorts the array
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        int min = arr[0];
+        int max = arr[arr.length-1];
+        System.out.print("max element : " + max + " minimum element : " + min);
+    }
+}
