aa975eb23fcdce451edc8f3397fa2a2e78d750f1
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-11 00:15:02 +0530

    arrayQue

--- Que6.java
+++ Que6.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class Que6 {
     // Find Union and Intersection of two arrays
     public static void main(String[] args) {
-        System.out.print("size of array1 and array2 : ");
+        System.out.print("size of array1 : ");
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int[] arr1 = new int[size];
+        int size1 = sc.nextInt();
+        int[] arr1 = new int[size1];
         System.out.println("elements of array1 :");
         for (int i = 0; i < arr1.length; i++) {
             arr1[i] = sc.nextInt();
         }
 
-        int[] arr2 = new int[size];
+        System.out.print("size of array2 : ");
+        int size2 = sc.nextInt();
+        int[] arr2 = new int[size2];
         System.out.println("elements of array2 :");
         for (int i = 0; i < arr2.length; i++) {
             arr2[i] = sc.nextInt();
@@ -21,10 +23,22 @@ public class Que6 {
         // union of two arrays
         int[] union = new int[arr1.length + arr2.length];
         int unionCount = 0;
+
+        // Add unique elements from arr1
         for (int i = 0; i < arr1.length; i++) {
-            union[unionCount++] = arr1[i];
+            boolean isDuplicate = false;
+            for (int j = 0; j < unionCount; j++) {
+                if (arr1[i] == union[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                union[unionCount++] = arr1[i];
+            }
         }
-        // For each element in arr2, check if it is already in union[]
+
+        // Add unique elements from arr2
         for (int i = 0; i < arr2.length; i++) {
             boolean isDuplicate = false;
             for (int j = 0; j < unionCount; j++) {
