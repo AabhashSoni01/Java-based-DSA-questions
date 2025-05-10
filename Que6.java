776fb048d30e36c50735e4af725c826905d5edd2
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-11 01:15:16 +0530

    arrayQue

--- Que6.java
+++ Que6.java
@@ -20,6 +20,7 @@ public class Que6 {
             arr2[i] = sc.nextInt();
         }
         sc.close();
+
         // union of two arrays
         int[] union = new int[arr1.length + arr2.length];
         int unionCount = 0;
@@ -51,9 +52,35 @@ public class Que6 {
                 union[unionCount++] = arr2[i];
             }
         }
+
+        // intersection of two arrays
+        int[] intersection = new int[Math.min(arr1.length, arr2.length)];
+        int intersecCount = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    boolean isAlreadyAdded = false;
+                    for (int k = 0; k < intersecCount; k++) {
+                        if (intersection[k] == arr1[i]) {
+                            isAlreadyAdded = true;
+                            break;
+                        }
+                    }
+                    if (!isAlreadyAdded) {
+                        intersection[intersecCount++] = arr1[i];
+                    }
+                    break;
+                }
+            }
+        }
+
         System.out.print("union of array: ");
         for (int i = 0; i < unionCount; i++) {
             System.out.print(union[i] + " ");
+        }
+        System.out.print("\nintersection of array: ");
+        for (int i = 0; i < intersecCount; i++) {
+            System.out.print(intersection[i] + " ");
         }
     }
 }
