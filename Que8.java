964504df09a8054e086f8e9273dbff3b06f3f233
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-13 23:44:00 +0530

    arrayQue

--- Que8.java
+++ Que8.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
+
 public class Que8 {
+
     // Kadane's Algorithm - Maximum Subarray Sum
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("size of array : ");
         int size = sc.nextInt();
         int[] arr = new int[size];
+
         System.out.println("array elements : ");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         sc.close();
+
         // to find the maximum sum of a subarray
         int currentMax = arr[0], maxEndsHere = arr[0];
         int start = 0, end = 0, subArrStart = 0;
+
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > (maxEndsHere + arr[i])) {
                 maxEndsHere = arr[i];
@@ -27,10 +33,12 @@ public class Que8 {
                 end = i;
             }
         }
+
         System.out.print("Subarray with maximum sum : ");
         for (int i = start; i <= end; i++) {
             System.out.print(arr[i] + " ");
         }
+
         System.out.println("\nMaximum Subarray Sum is : " + currentMax);
     }
 }
