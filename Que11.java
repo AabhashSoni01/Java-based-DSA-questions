6b7fbf64b2e42b537f67f077064330559af13997
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-15 22:54:27 +0530

    arrayQue

---
+++ Que11.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Que11 {
+
+    // Rearrange array in alternating positive & negative items
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("size of array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("array elements : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+    }
+}
