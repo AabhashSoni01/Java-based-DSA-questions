2627a6b7a9ce30a4ff81e4fffe49ecd0381c86f8
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-17 15:29:24 +0530

    arrayQue

--- Que12.java
+++ Que12.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
+
 public class Que12 {
 
     // Count the number of occurrences of an element
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("size of array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("array elements : ");
+        for(int i=0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("element, whose occurance is to be count : ");
+        int element = sc.nextInt();
+        sc.close();
+
+        // counts the occurance of an element in the array
+        int count = 0;
+
+        for(int i=0; i<size; i++){
+            if(arr[i] == element){
+                count++;
+            }
+        }
+
+        System.out.println("The element " + element + " occurs " + count + " times in the array.\n");
+    }
 }
