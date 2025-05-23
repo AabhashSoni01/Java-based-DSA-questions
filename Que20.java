288eb1c44027b4b79e60d14c5b38676956539061
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-23 23:31:50 +0530

    arrayQue

--- Que20.java
+++ Que20.java
@@ -1,5 +1,36 @@
-public class Que20 {
+import java.util.Scanner;
 
+public class Que20 {
     // Maximum Product Subarray
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("size of array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("array elements : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        int maxProduct = arr[0], minProduct = arr[0], overallMax = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+
+            int current = arr[i];
+
+            if (current < 0) {
+                // Swap max and min when encountering a negative number
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
+            }
+
+            maxProduct = Math.max(current, current * maxProduct);
+            minProduct = Math.min(current, current * minProduct);
+
+            overallMax = Math.max(overallMax, maxProduct);
+        }
+        System.out.println("Max product : " + overallMax);
+    }
 }
