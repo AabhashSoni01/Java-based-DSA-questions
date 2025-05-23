5ab332f5c58f3d7479a0dfd9c930812bfadc49a2
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-24 01:38:36 +0530

    stringQue

--- Que21.java
+++ Que21.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Que21 {
-    
+
     // Reverse a string
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a string : ");
+        String str = sc.nextLine();
+        sc.close();
+
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        System.out.print("reversed string : " + reversed);
+    }
 }
