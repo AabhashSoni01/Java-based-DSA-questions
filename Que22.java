1b7993cd105a8592cf6df16a90a0c45ea18dbbd8
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-24 02:14:06 +0530

    stringQue

---
+++ Que22.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Que22 {
+
+    // Check for palindrome
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a string : ");
+        String str = sc.nextLine();
+        sc.close();
+
+        boolean isPalindrome = true;
+        int left = 0, right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("string is a palindrom\n");
+        } else {
+            System.out.println("string is not a palindrom\n");
+        }
+    }
+}
