424fa10950aa7773bf3e861189d6aeeea9489950
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-25 00:24:29 +0530

    stringQue

--- Que23.java
+++ Que23.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Que23 {
-    
+
     // Remove duplicates from a string
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("original string : ");
+        String str = sc.nextLine();
+        sc.close();
+
+        boolean[] present = new boolean[256]; // for ASCII
+        StringBuilder result = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (!present[c]) {
+                present[c] = true;
+                result.append(c);
+            }
+        }
+        System.out.print("string with duplicates removed : " + result);
+    }
 }
