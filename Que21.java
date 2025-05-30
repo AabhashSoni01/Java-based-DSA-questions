6a5fd98c941fbcc3dde9e23fe30a373b6f3afa81
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-05-30 18:24:50 +0530

    stringQue

--- Que21.java
+++ Que21.java
@@ -10,9 +10,9 @@ public class Que21 {
         String str = sc.nextLine();
         sc.close();
 
-        String reversed = "";
+        StringBuilder reversed = new StringBuilder();
         for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
+            reversed.append(str.charAt(i));
         }
         System.out.print("reversed string : " + reversed);
     }
