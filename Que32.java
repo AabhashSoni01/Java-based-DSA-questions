24fb7ffd06761487c739f5383ca989aa4db729b7
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-06-05 23:59:36 +0530

    stringQue

--- Que32.java
+++ Que32.java
@@ -1,3 +1,38 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Que32 {
-    
+    // Longest substring without repeating characters
+    public static String Longestubstring(String str) {
+        HashSet<Character> set = new HashSet<>();
+        int start = 0, maxLength = 0, maxStringStart = 0;
+
+        for (int end = 0; end < str.length(); end++) {
+            char currentChar = str.charAt(end);
+
+            while (set.contains(currentChar)) {
+                set.remove(currentChar);
+                start++;
+            }
+            set.add(currentChar);
+
+            // Update maxLength and starting index
+            if (end - start + 1 > maxLength) {
+                maxLength = end - start + 1;
+                maxStringStart = start;
+            }
+        }
+        return str.substring(maxStringStart, maxStringStart + maxLength);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string : ");
+        sc.nextLine(); // consume the leftover newline
+        String str = sc.nextLine();
+        sc.close();
+        String result = Longestubstring(str);
+        System.err.println("Longest substring without repeating characters: " + result);
+        System.out.println("Length: " + result.length());
+    }
 }
