ed781a63da521c70a11661c395b0af41468dec45
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-06-06 23:42:56 +0530

    stringQue

---
+++ Que33.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Que33 {
+    // Group anagrams together
+
+    public static void main(String[] args) {
+        System.out.print("Enter the number of strings : ");
+        Scanner sc = new Scanner(System.in);
+        int no = sc.nextInt();
+        sc.nextLine();
+        String[] strs = new String[no];
+
+        System.out.println("Enter strings : ");
+        for (int i = 0; i < strs.length; i++) {
+            strs[i] = sc.nextLine();
+        }
+        sc.close();
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray(); // Convert to char array
+            Arrays.sort(chars); // then sort
+            String sortedWord = new String(chars); // Convert back to strin
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>()); // If sortedWord not in map, add to map
+            }
+            map.get(sortedWord).add(word); // Add original word to map.get(sortedWord)
+        }
+
+        System.out.println("Grouped anagrams : ");
+        for (List<String> s : map.values()) {
+            System.out.println(s);
+        }
+    }
+}
