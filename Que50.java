46cb058d404e3e9e18cdc8ae94c7f66613242ecb
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-06-23 20:56:37 +0530

    2dArray

--- Que50.java
+++ Que50.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Que50 {
-    
+
     // Boolean Matrix problem (set row/column to 1 if any element is 1)
-    
+
+    public static void main(String[] args) {
+        System.out.print("Enter the number of rows : ");
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns : ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Enter elements of the matrix : ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+    }
+
 }
