e8e1aa82988757b9d0fbf42cdefc6fb284c8786f
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-06-23 21:02:25 +0530

    2dArray

--- Que50.java
+++ Que50.java
@@ -28,18 +28,55 @@ public class Que50 {
             System.out.println();
         }
 
-        boolean zeroInFirstRow = false, zeroInFirstCol = false;
+        boolean oneInFirstRow = false, oneInFirstCol = false;
 
         for (int i = 0; i < rows; i++) {
-            if (matrix[i][0] == 0) {
-                zeroInFirstCol = true;
+            if (matrix[i][0] == 1) {
+                oneInFirstCol = true;
             }
         }
 
         for (int j = 0; j < cols; j++) {
-            if (matrix[0][j] == 0) {
-                zeroInFirstRow = true;
+            if (matrix[0][j] == 1) {
+                oneInFirstRow = true;
             }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][0] = 1;
+                    matrix[0][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][0] == 1 || matrix[0][j] == 1) {
+                    matrix[i][j] = 1;
+                }
+            }
+        }
+
+        if (oneInFirstRow) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 1;
+            }
+        }
+
+        if (oneInFirstCol) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 1;
+            }
+        }
+
+        System.out.println("New matrix : ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 
